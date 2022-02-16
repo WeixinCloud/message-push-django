f3f911149d3f1c26738c77f1db95484259f26271
voyagerma <m17793873123@163.com>
2022-02-16 16:25:34 +0800

    修复代码规范问题

--- wxcloudrun/settings.py
+++ wxcloudrun/settings.py
@@ -2,9 +2,10 @@ import os
 from pathlib import Path
 import time
 
-CUR_PATH = os.path.dirname(os.path.realpath(__file__))  
-LOG_PATH = os.path.join(os.path.dirname(CUR_PATH), 'logs') # LOG_PATH是存放日志的路径
-if not os.path.exists(LOG_PATH): os.mkdir(LOG_PATH)  # 如果不存在这个logs文件夹，就自动创建一个
+CUR_PATH = os.path.dirname(os.path.realpath(__file__))
+LOG_PATH = os.path.join(os.path.dirname(CUR_PATH), 'logs')
+if not os.path.exists(LOG_PATH):
+    os.mkdir(LOG_PATH)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
